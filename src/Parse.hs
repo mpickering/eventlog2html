5a1849f7aac3d35779d133c246ca95cc3893b0a8
Claude Heiland-Allen <claudiusmaximus@goto10.org>
2010-07-26 17:29:29 +0100

    eliminate duplicate strings using a state monad for improved memory usage

--- src/Parse.hs
+++ src/Parse.hs
@@ -1,7 +1,9 @@
 module Parse where
 
+import Control.Monad.State.Strict(State(), evalState, get, put)
+import Control.Monad(foldM)
 import Data.List(isPrefixOf)
-import Data.Map(Map, empty, insertWith')
+import Data.Map(Map, empty, insertWith', alter, (!))
 
 import Types
 
@@ -11,7 +13,7 @@ parse s =
       (hs, ss) = splitAt 4 ls
       [job, date, smpU, valU] =
         zipWith header ["JOB", "DATE", "SAMPLE_UNIT", "VALUE_UNIT"] hs
-      frames = map parseFrame . chunkSamples1 $ ss
+      frames = flip evalState empty . mapM parseFrame . chunkSamples1 $ ss
   in  Run
       { hprJob        = job
       , hprDate       = date
@@ -40,9 +42,9 @@ chunkSamples2 (x:xs) =
   then ([x],xs)
   else let (ys,zs) = chunkSamples2 xs in (x:ys, zs)
 
-parseFrame :: [String] -> Frame
+parseFrame :: [String] -> State (Map String String) Frame
 parseFrame [] = error "Parse.parseFrame: empty"
-parseFrame (l:ls) =
+parseFrame (l:ls) = do
   let begin = sampleTime "BEGIN_SAMPLE" l
       end   = sampleTime "END_SAMPLE" $ last ls
       smps  = init ls
@@ -50,17 +52,23 @@ parseFrame (l:ls) =
         if begin == end
         then begin
         else error "Parse.parseFrame: begin /= end"
-      samples = foldr inserter empty smps
-  in  Frame
-      { hpfTime    = time
-      , hpfSamples = samples
-      }
+  samples <- foldM inserter empty smps
+  return  Frame
+          { hpfTime    = time
+          , hpfSamples = samples
+          }
 
-inserter :: String -> Map String Double -> Map String Double
-inserter s m =
+inserter :: Map String Double -> String -> State (Map String String) (Map String Double)
+inserter m s = do
   let [k,vs] = words s
       v = read vs
-  in  insertWith' (+) k v m
+  get >>= put . alter (alterer k) k
+  names <- get
+  return $ insertWith' (+) (names ! k) v m
+
+alterer :: String -> Maybe String -> Maybe String
+alterer s Nothing = Just s
+alterer _ js      = js
 
 sampleTime :: String -> String -> Double
 sampleTime name h =
