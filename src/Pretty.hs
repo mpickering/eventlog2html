c9ad2d14aaf72660bc0d85d984bb2fde22dbd0a1
Claude Heiland-Allen <claudiusmaximus@goto10.org>
2010-07-28 17:35:16 +0100

    accumulate with arrays instead of transposed lists

--- src/Pretty.hs
+++ src/Pretty.hs
@@ -1,7 +1,10 @@
 module Pretty where
 
-import Data.List (transpose)
+import Control.Monad (forM_, liftM2)
+import Data.Array.ST
+import Data.Array.Unboxed (UArray)
 import Data.ByteString.Lazy.Char8 (pack)
+--import Data.List (transpose)
 
 import Types
 
@@ -11,7 +14,7 @@ pretty hpi =
       vticks = uncurry (ticks 20) (hpiValueRange hpi)
       labels = pack "(trace elements)" : (reverse . map fst . hpiValues) hpi
       values = hpiTrace hpi : (reverse . map snd . hpiValues) hpi
-      bands  = transpose . map (scanl (+) 0) . transpose $ values
+      bands  = accumulate' values
   in  Graph
       { hpgJob        = hpiJob hpi
       , hpgDate       = hpiDate hpi
@@ -25,6 +28,22 @@ pretty hpi =
       , hpgBands      = bands
       , hpgSamples    = hpiSamples hpi
       }
+
+{-
+accumulate :: [[Double]] -> [[Double]]
+accumulate = transpose . map (scanl (+) 0) . transpose
+-}
+
+accumulate' :: [[Double]] -> UArray (Int, Int) Double
+accumulate' [] = error $ "accumulate': empty"
+accumulate' xss@(x:_) = runSTUArray $ do
+  let bands   = length xss
+      samples = length x
+  a <- newListArray ((0,1),(bands,samples)) $ replicate samples 0 ++ concat xss
+  forM_ [1 .. samples] $ \s ->
+    forM_ [1 .. bands] $ \b ->
+      writeArray a (b, s) =<< liftM2 (+) (readArray a (b - 1, s)) (readArray a (b, s))
+  return a
 
 ticks :: Int -> Double -> Double -> [Double]
 ticks n mi ma =
