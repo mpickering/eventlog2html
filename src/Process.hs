53e6f021dcdd3e890aee2d44244a52fd6440453a
Claude Heiland-Allen <claudiusmaximus@goto10.org>
2010-07-28 23:16:29 +0100

    use computed totals from the parser

--- src/Process.hs
+++ src/Process.hs
@@ -1,8 +1,8 @@
 module Process where
 
-import Control.Arrow((&&&))
-import Data.List (sortBy)
-import Data.Map (unionsWith, toList, fromList, difference, elems, findWithDefault)
+import Control.Arrow ((&&&))
+import Data.List (foldl', sortBy)
+import Data.Map (toList, fromList, difference, elems, findWithDefault)
 import Data.Ord (comparing)
 
 import Types
@@ -11,18 +11,18 @@ process :: Run -> Info
 process hpr =
   let frames = hprFrames hpr
       samples = map hpfSamples frames
-      ccTotals = sortBy (flip $ comparing snd) . toList . unionsWith (+) $ samples
+      ccTotals = sortBy (flip $ comparing snd) (toList $ hprTotals hpr)
       sizes = map snd ccTotals
-      total = sum sizes
+      total = sum' sizes
       limit = 0.99 * total
       bigs = takeWhile (< limit) . drop 1 . scanl (+) 0 $ sizes
-      bands = take (15 `max` length bigs) ccTotals
+      bands = zipWith const ccTotals $ take 15 bigs
       ccs = map fst bands
       ccsMap = fromList . map (\c -> (c, ())) $ ccs
       sr = (minimum &&& maximum) . map hpfTime $ frames
       vr = (0, maximum . map (sum . elems) $ samples)
       values = [ (c, [ findWithDefault 0 c m | m <- samples ]) | c <- ccs ]
-      traces = map (sum . elems . (`difference` ccsMap)) samples
+      traces = map (sum' . elems . (`difference` ccsMap)) samples
   in  Info
       { hpiJob        = hprJob hpr
       , hpiDate       = hprDate hpr
@@ -34,3 +34,6 @@ process hpr =
       , hpiValues     = values
       , hpiTrace      = traces
       }
+
+sum' :: [Double] -> Double
+sum' = foldl' (+) 0
