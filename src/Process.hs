0e538a24c72eb9c27d9eb9fd937cf550bdbd4abb
Claude Heiland-Allen <claudiusmaximus@goto10.org>
2010-07-30 17:44:28 +0100

    attempt to process frames in a single pass to reduce memory consumption

--- src/Process.hs
+++ src/Process.hs
@@ -1,37 +1,37 @@
 module Process (process) where
 
-import Control.Arrow ((&&&))
-import Data.List (foldl', sortBy)
-import Data.Map (toList, fromList, difference, elems, findWithDefault)
+import Control.Arrow (second)
+import Data.List (foldl', sortBy, partition)
 import Data.Ord (comparing)
+import qualified Data.Map as M
+import qualified Data.Set as S
 
 import Types
 
 process :: Run -> Info
 process r =
   let frames = rFrames r
-      samples = map fSamples frames
-      ccTotals = sortBy (flip $ comparing snd) (toList $ rTotals r)
+      ccTotals = sortBy (flip $ comparing snd) (M.toList $ rTotals r)
       sizes = map snd ccTotals
       total = sum' sizes
       limit = 0.99 * total
       bigs = takeWhile (< limit) . drop 1 . scanl (+) 0 $ sizes
       bands = zipWith const ccTotals $ take 15 bigs
       ccs = map fst bands
-      ccsMap = fromList . map (\c -> (c, ())) $ ccs
-      sr = (minimum &&& maximum) . map fTime $ frames
-      vr = (0, maximum . map (sum' . elems) $ samples)
-      values = [ (c, [ findWithDefault 0 c m | m <- samples ]) | c <- ccs ]
-      traces = map (sum' . elems . (`difference` ccsMap)) samples
+      ccsS = S.fromList ccs
+      (times, (keep, rest)) = second unzip . unzip . map (second (partition ((`S.member` ccsS) . fst))) $ frames
+      smps = map M.fromList keep
+      values = [ (c, [ M.findWithDefault 0 c m | m <- smps ]) | c <- ccs ]
+      traces = map (sum' . map snd) rest
   in  Info
       { iJob         = rJob r
       , iDate        = rDate r
       , iSampleUnit  = rSampleUnit r
       , iValueUnit   = rValueUnit r
-      , iSampleRange = sr
-      , iValueRange  = vr
+      , iSampleRange = rSampleRange r
+      , iValueRange  = rValueRange r
       , iCount       = rCount r
-      , iSamples     = map fTime frames
+      , iSamples     = times
       , iValues      = values
       , iTrace       = traces
       }
