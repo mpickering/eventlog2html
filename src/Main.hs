612f4dcf48b3b06911446b6c836434c03e07f386
Claude Heiland-Allen <claudiusmaximus@goto10.org>
2010-07-30 02:46:28 +0100

    trying to fix somewhere that retains too much list

--- src/Main.hs
+++ src/Main.hs
@@ -1,12 +1,16 @@
 module Main (main) where
 
-import Prelude hiding (print, interact)
-import Data.ByteString.Lazy.Char8 (interact)
+import Prelude hiding (print, getContents, putStr, interact)
+import Data.ByteString.Lazy.Char8 (ByteString, getContents, putStr)
 
 import Parse (parse)
 import Process (process)
 import Pretty (pretty)
 import Print (print)
+import SVG (svg)
 
 main :: IO ()
-main = interact $ print . pretty . process . parse
+main = interact $ print svg . pretty . process . parse
+
+interact :: (ByteString -> [ByteString]) -> IO ()
+interact f = getContents >>= mapM_ putStr . f
