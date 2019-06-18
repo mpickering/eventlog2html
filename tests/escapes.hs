20c5ffdeb6ab18713a12c27ea92012ef8aba8482
BinderDavid <binderd@informatik.uni-tuebingen.de>
2019-06-18 23:31:57 +0200

    Removed old examples, tests and toplevel files

--- tests/escapes.hs
+++
@@ -1,8 +0,0 @@
-module Main where
-
-a <<< b = reverse a ++ reverse b
-a &&& b = zipWith (\x y -> [x, y]) a b
-foo = {-# SCC "&<>!'\"/" #-} id
-
-main = interact main'
-main' s = foo $ (s <<< concat (s &&& s)) <<< concat (s &&& s) <<< s
