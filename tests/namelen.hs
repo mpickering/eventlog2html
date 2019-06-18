3ed29573a1af79c505d387ee6a5aec30c9d2be42
BinderDavid <binderd@informatik.uni-tuebingen.de>
2019-06-18 23:31:57 +0200

    Removed old examples, tests and toplevel files

--- tests/namelen.hs
+++
@@ -1,8 +0,0 @@
-module Main where
-
-a <<< b = {-# SCC "ThisSCCHasAnExtremelyLongNameThatMightBeChoppedOffPrematurely" #-} reverse a ++ reverse b
-a &&& b = zipWith (\x y -> {-# SCC "ThisSCCHasAnotherExtremelyLongNameThatMightBeChoppedOffPrematurely" #-} [x, y]) a b
-foo = id
-
-main = interact main'
-main' s = foo $ (s <<< concat (s &&& s)) <<< concat (s &&& s) <<< s
