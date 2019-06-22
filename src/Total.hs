8003a402f49a5660ea8daa27c854bc4c447ac8d5
BinderDavid <david.binder@uni-tuebingen.de>
2019-06-22 21:58:46 +0200

    Safe Removal of computed ranges

--- src/Total.hs
+++ src/Total.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE BangPatterns #-}
 module Total (total) where
 
-import Control.Monad.State.Strict (State(), execState, get, put)
+import Control.Monad.State.Strict (State(), execState, get, put, modify)
 import Data.List (foldl')
 import Data.Map (Map, empty, lookup, insert, alter)
 import Prelude hiding (init, lookup, lines, words, drop, length, readFile)
@@ -14,25 +14,17 @@ data Parse =
   Parse
   { symbols   :: !(Map Text Text) -- intern symbols to save RAM
   , totals    :: !(Map Text (Double, Double)) -- compute running totass and total of squares
-  , sampleMin :: !Double
-  , sampleMax :: !Double
-  , valueMin  :: !Double
-  , valueMax  :: !Double
   , count     :: !Int                         -- number of frames
   }
 
 parse0 :: Parse
-parse0 = Parse{ symbols = empty, totals = empty, sampleMin = 0, sampleMax = 0, valueMin = 0, valueMax = 0, count = 0 }
-
-
+parse0 = Parse{ symbols = empty, totals = empty, count = 0 }
 
 total :: PartialHeader -> [Frame] -> (Header, Map Text (Double, Double))
 total ph fs =
   let parse1 = flip execState parse0 . mapM_ parseFrame $ fs
   in  (
-       ph (sampleMin parse1, sampleMax parse1)
-          (valueMin parse1, valueMax parse1)
-          (count parse1)
+       ph (count parse1)
       , fmap (stddev $ fromIntegral (count parse1)) (totals parse1)
       )
 
@@ -41,15 +33,9 @@ stddev s0 (s1, s2) = (s1, sqrt (s0 * s2 - s1 * s1) / s0)
 
 
 parseFrame :: Frame -> State Parse ()
-parseFrame (Frame time ls) = do
+parseFrame (Frame _time ls) = do
   samples <- mapM inserter ls
-  p <- get
-  let v = foldl' (+) 0 samples
-      sMin = if count p == 0 then time else time `min` sampleMin p
-      sMax = if count p == 0 then time else time `max` sampleMax p
-      vMin = v `min` valueMin p
-      vMax = v `max` valueMax p
-  put $! p{ count = count p + 1, sampleMin = sMin, sampleMax = sMax, valueMin = vMin, valueMax = vMax }
+  modify $ \p -> p{ count = count p + 1 }
 
 inserter :: Sample -> State Parse Double
 inserter (Sample k v) = do
--- src/Types.hs
+++ src/Types.hs
@@ -8,12 +8,10 @@ data Header =
   , hDate        :: Text
   , hSampleUnit  :: Text
   , hValueUnit   :: Text
-  , hSampleRange :: (Double, Double)
-  , hValueRange  :: (Double, Double)
   , hCount       :: Int
   }
 
-type PartialHeader = (Double, Double) -> (Double, Double) -> Int -> Header
+type PartialHeader = Int -> Header
 
 data Sample = Sample Text Double deriving Show
 
