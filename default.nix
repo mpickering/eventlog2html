8e26648e942bba71eb2046483f26254b39ea3fbe
Matthew Pickering <matthewtpickering@gmail.com>
2019-09-06 14:44:11 +0100

    Update CI to 5th September

--- default.nix
+++ default.nix
@@ -4,12 +4,12 @@ let
   haskell = import ((import ./nix/sources.nix)."haskell.nix") { pkgs = pin; };
 
   pkgPlan = haskell.callCabalProjectToNix
-              { index-state = "2019-05-10T00:00:00Z"
+              { index-state = "2019-09-05T00:00:00Z"
               ; src = pin.lib.cleanSource ./.;};
 
   # Instantiate a package set using the generated file.
   pkgSet = haskell.mkCabalProjectPkgSet {
-    plan-pkgs = import pkgPlan;
+    plan-pkgs = import pkgPlan.projectNix;
     pkg-def-extras = [];
     modules = [];
   };
