e949f18f0db259752cc2caf50d8581b6bcf3d957
BinderDavid <david.binder@uni-tuebingen.de>
2019-09-08 12:48:10 +0200

    Added check for GHC Version

--- src/Eventlog/Events.hs
+++ src/Eventlog/Events.hs
@@ -36,6 +36,21 @@ eventlogToHP :: EventLog -> IO (PartialHeader, [Frame], [Trace])
 eventlogToHP (EventLog _h e) = do
   eventsToHP e
 
+checkGHCVersion :: EL -> Maybe String
+checkGHCVersion EL { ident = Just (version,_)}  | version <= makeVersion [8,4,4]  =
+                                                  Just $ "Warning: The eventlog has been generated with GHC version "
+                                                  ++ show version
+                                                  ++ ", which does not support profiling events in the eventlog."
+checkGHCVersion EL { pargs = Just args, ident = Just (version,_)}| version > makeVersion [8,4,4] &&
+                                                                   version <= makeVersion [8,9,0] &&
+                                                                   ("-hr" `elem` args ||
+                                                                    "-hb" `elem` args) =
+                                                                   Just $ "The eventlog has been generated with GHC version"
+                                                                   ++ show version
+                                                                   ++ ", which does not support biographical or retainer profiling."
+checkGHCVersion _ = Nothing
+
+
 eventsToHP :: Data -> IO (PartialHeader, [Frame], [Trace])
 eventsToHP (Data es) = do
   let
