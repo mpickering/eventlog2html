a13292f401c01aaf7b4393a3cb86b63580156a1e
BinderDavid <binderd@informatik.uni-tuebingen.de>
2019-09-09 14:10:59 +0200

    Cleaned up backend code

--- src/Eventlog/Data.hs
+++ src/Eventlog/Data.hs
@@ -3,36 +3,23 @@ module Eventlog.Data (generateJson) where
 
 import Prelude hiding (print, readFile)
 import Data.Aeson (Value(..), (.=), object)
-import Data.Tuple (swap)
 
-import Eventlog.Args (Args(..), Sort(..))
+import Eventlog.Args (Args(..))
 import Eventlog.Bands (bands)
 import qualified Eventlog.Events as E
 import qualified Eventlog.HeapProf as H
-import Eventlog.Prune (prune, cmpName, cmpSize, cmpStdDev)
+import Eventlog.Prune (prune)
 import Eventlog.Vega
 import Eventlog.Types (Header)
 
 generateJson :: FilePath -> Args -> IO (Header, Value)
 generateJson file a = do
   let chunk = if heapProfile a then H.chunk else E.chunk
-      cmp = fst $ reversing' sorting'
-      sorting' = case sorting a of
-        Name -> cmpName
-        Size -> cmpSize
-        StdDev -> cmpStdDev
-      reversing' = if reversing a then swap else id
   (h,totals, fs, traces) <- chunk file
-  let keeps = prune cmp 0 (bound $ nBands a) totals
-      filtered_traces = if noTraces a then [] else traces
+  let keeps = prune a 0 totals
   let combinedJson = object [
           "samples" .= bandsToVega keeps (bands h keeps fs)
-        , "traces"  .= tracesToVega filtered_traces
+        , "traces"  .= tracesToVega traces
         ]
   return (h, combinedJson)
-
-bound :: Int -> Int
-bound n
-  | n <= 0 = maxBound
-  | otherwise = n
 
