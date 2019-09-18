mpickering/eventlog2html

commit c3cdfd0547995359904f7610989b08b0a2e68c75

Cleaned up backend code

src/Eventlog/Prune.hs
@@ -1,9 +1,5 @@
 module Eventlog.Prune
   ( prune
-  , Compare
-  , cmpName
-  , cmpSize
-  , cmpStdDev
   ) where
 
 import Data.Text (Text)
@@ -11,13 +7,17 @@ import Data.List (foldl', sortBy)
 import Data.Ord (comparing)
 import Data.Map.Strict (Map, toList, fromList)
 
+import Eventlog.Args (Args(..), Sort(..))
+
 type Compare a = a -> a -> Ordering
 
-cmpName, cmpSize, cmpStdDev
-  :: (Compare (Text, (Double, Double)), Compare (Text, (Double, Double)))
-cmpName = (cmpNameAscending, cmpNameDescending)
-cmpSize = (cmpSizeDescending, cmpSizeAscending)
-cmpStdDev = (cmpStdDevDescending, cmpStdDevAscending)
+getComparison :: Args -> Compare (Text, (Double, Double))
+getComparison Args { sorting = Size,   reversing = True }  = cmpSizeDescending
+getComparison Args { sorting = Size,   reversing = False } = cmpSizeAscending
+getComparison Args { sorting = StdDev, reversing = True }  = cmpStdDevDescending
+getComparison Args { sorting = StdDev, reversing = False } = cmpStdDevAscending
+getComparison Args { sorting = Name,   reversing = True }  = cmpNameDescending
+getComparison Args { sorting = Name,   reversing = False } = cmpNameAscending
 
 cmpNameAscending, cmpNameDescending,
   cmpStdDevAscending, cmpStdDevDescending,
@@ -29,17 +29,22 @@ cmpStdDevDescending = flip cmpStdDevAscending
 cmpSizeAscending = comparing (fst . snd)
 cmpSizeDescending = flip cmpSizeAscending
 
-prune :: Compare (Text, (Double, Double)) -> Double -> Int -> Map Text (Double, Double) -> Map Text Int
-prune cmp tracePercent nBands ts =
+prune :: Args -> Double -> Map Text (Double, Double) -> Map Text Int
+prune args tracePercent ts =
   let ccTotals = sortBy cmpSizeDescending (toList ts)
       sizes = map (fst . snd) ccTotals
       total = sum' sizes
       limit = if tracePercent == 0 then total
                                    else (1 - tracePercent / 100) * total
       bigs = takeWhile (< limit) . scanl (+) 0 $ sizes
-      bands = zipWith const ccTotals $ take nBands bigs
-      ccs = map fst (sortBy cmp bands)
+      bands = zipWith const ccTotals $ take (bound $ nBands args) bigs
+      ccs = map fst (sortBy (getComparison args) bands)
   in  fromList (reverse ccs `zip` [1 ..])
+
+bound :: Int -> Int
+bound n
+  | n <= 0 = maxBound
+  | otherwise = n
 
 sum' :: [Double] -> Double
 sum' = foldl' (+) 0

src/Eventlog/Data.hs
@@ -3,35 +3,23 @@ module Eventlog.Data (generateJson) where
 
 import Prelude hiding (print, readFile)
 import Data.Aeson (Value(..), (.=), object)
-import Data.Tuple (swap)
 
-import Eventlog.Args (Args(..), Sort(..))
+import Eventlog.Args (Args(..))
 import Eventlog.Bands (bands)
 import qualified Eventlog.Events as E
 import qualified Eventlog.HeapProf as H
-import Eventlog.Prune (prune, cmpName, cmpSize, cmpStdDev)
+import Eventlog.Prune (prune)
 import Eventlog.Vega
 import Eventlog.Types (Header)
 
 generateJson :: FilePath -> Args -> IO (Header, Value)
 generateJson file a = do
   let chunk = if heapProfile a then H.chunk else E.chunk a
-      cmp = fst $ reversing' sorting'
-      sorting' = case sorting a of
-        Name -> cmpName
-        Size -> cmpSize
-        StdDev -> cmpStdDev
-      reversing' = if reversing a then swap else id
   (h,totals, fs, traces) <- chunk file
-  let keeps = prune cmp 0 (bound $ nBands a) totals
+  let keeps = prune a 0 totals
   let combinedJson = object [
           "samples" .= bandsToVega keeps (bands h keeps fs)
         , "traces"  .= tracesToVega traces
         ]
   return (h, combinedJson)
-
-bound :: Int -> Int
-bound n
-  | n <= 0 = maxBound
-  | otherwise = n
 

commit d3960a26c9e731c2d3302d77a638dd6248471579

Fix ordering of bands
This mistake was introduced in

c3cdfd0547995359904f7610989b08b0a2e68c75

src/Eventlog/Prune.hs
@@ -12,10 +12,10 @@ import Eventlog.Args (Args(..), Sort(..))
 type Compare a = a -> a -> Ordering
 
 getComparison :: Args -> Compare (Text, (Double, Double))
-getComparison Args { sorting = Size,   reversing = True }  = cmpSizeDescending
-getComparison Args { sorting = Size,   reversing = False } = cmpSizeAscending
-getComparison Args { sorting = StdDev, reversing = True }  = cmpStdDevDescending
-getComparison Args { sorting = StdDev, reversing = False } = cmpStdDevAscending
+getComparison Args { sorting = Size,   reversing = False }  = cmpSizeDescending
+getComparison Args { sorting = Size,   reversing = True } = cmpSizeAscending
+getComparison Args { sorting = StdDev, reversing = False }  = cmpStdDevDescending
+getComparison Args { sorting = StdDev, reversing = True } = cmpStdDevAscending
 getComparison Args { sorting = Name,   reversing = True }  = cmpNameDescending
 getComparison Args { sorting = Name,   reversing = False } = cmpNameAscending
 