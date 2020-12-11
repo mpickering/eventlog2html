6c774384f33ab4428538b50fad400e9bf484a5f1
Matthew Pickering <matthewtpickering@gmail.com>
2020-12-11 11:00:24 +0000

    update shell.nix

--- shell.nix
+++ shell.nix
@@ -1,5 +1,5 @@
 with { pkgs = import (import ./nix/sources.nix).nixpkgs {}; };
 pkgs.mkShell
-  { buildInputs = [ pkgs.haskell.compiler.ghc881 pkgs.zlib pkgs.haskellPackages.cabal-install pkgs.git-lfs ];
+  { buildInputs = [ pkgs.haskell.compiler.ghc883 pkgs.zlib pkgs.haskellPackages.cabal-install pkgs.git-lfs ];
     shellHook=''export LD_LIBRARY_PATH=${pkgs.gmp}/lib:${pkgs.zlib}/lib:${pkgs.ncurses}/lib'';
   }
