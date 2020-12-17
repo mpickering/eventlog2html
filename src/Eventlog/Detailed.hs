c5f3a67fb5bc01ac2730f2aa17754c16463f235a
David Eichmann <EichmannD@gmail.com>
2020-12-17 11:17:10 +0000

    Bug fix: invalid fancy table initialization

--- src/Eventlog/Detailed.hs
+++ src/Eventlog/Detailed.hs
@@ -117,7 +117,7 @@ renderSpark vs = H.span ! A.class_ "linechart" $ toHtml (T.intercalate "," (map 
 initTable :: Bool -> T.Text
 initTable b = "$(document).ready(function() {\
         \$(\".closureTable\").fancyTable({\
-        \    sortColumn:" <> if b then "8" else "3" <> ",\
+        \    sortColumn:" <> (if b then "8" else "3") <> ",\
         \    pagination: true,\
         \    perPage:10,\
         \    globalSearch:false,\
