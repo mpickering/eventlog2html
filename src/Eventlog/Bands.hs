892b3470b54c570ffe34ab68420b11553f4aecf2
Ellie Hermaszewska <git@monoid.al>
2021-11-09 15:19:31 +0800

    Disambiguate Data.Map.size
    hashtables 1.2.4.2 added size
    
    See also https://github.com/mpickering/eventlog2html/issues/152

--- src/Eventlog/Bands.hs
+++ src/Eventlog/Bands.hs
@@ -20,7 +20,7 @@ import Data.Set (Set, notMember)
 bands :: Header -> Map Bucket Int -> [Frame] -> (UArray Int Double, UArray (Int, Int) Double)
 bands h bs frames = runST $ do
   times <- newArray (1, hCount h) 0
-  vals  <- newArray ((-1,1), (size bs, hCount h)) 0
+  vals  <- newArray ((-1,1), (Data.Map.size bs, hCount h)) 0
   forM_ (zip [1 ..] frames) $ \(i, (Frame t ss)) -> do
     writeArray times i t
     forM_ ss $ \(Sample k v) -> do
